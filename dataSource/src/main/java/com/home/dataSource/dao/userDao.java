f893bca0e6a4b31a72e11267ff337ea896af7b84
jotalasx <jotalasx@gmail.com>
2019-03-26 16:32:53 +0100

    Testing over

---
+++ dataSource/src/main/java/com/home/dataSource/dao/userDao.java
@@ -0,0 +1,94 @@
+package com.home.dataSource.dao;
+
+public class userDao {
+	
+	private Integer id;
+	private String firstname, lastname, phonenumber;
+	public userDao(String firstname, String lastname, String phonenumber) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.phonenumber = phonenumber;
+	}
+	public userDao(Integer id, String firstname, String lastname, String phonenumber) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.phonenumber = phonenumber;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	@Override
+	public String toString() {
+		return "userDao [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", phonenumber="
+				+ phonenumber + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((phonenumber == null) ? 0 : phonenumber.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		userDao other = (userDao) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (phonenumber == null) {
+			if (other.phonenumber != null)
+				return false;
+		} else if (!phonenumber.equals(other.phonenumber))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
